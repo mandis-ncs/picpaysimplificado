e8f917930b0ccfe87aba8e673796943c16cbee7c
mandis-ncs <amnd.castro@gmail.com>
2024-06-25 15:25:41 -0300

    fix: fixing null return parameters

--- src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
@@ -40,10 +40,10 @@ public class User {
     public User(UserDTO data){
         this.firstName = data.firstName();
         this.lastName = data.lastName();
-        this.document = data.document();
         this.balance = data.balance();
         this.userType = data.userType();
         this.password = data.password();
+        this.document = data.document();
         this.email = data.email();
     }
 }
