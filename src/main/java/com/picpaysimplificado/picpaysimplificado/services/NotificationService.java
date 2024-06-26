42e41c5358dc293ea344598aaa23cae2c688dbad
mandis-ncs <amnd.castro@gmail.com>
2024-06-26 10:18:00 -0300

    feat: creating notify url variable

--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
@@ -3,6 +3,7 @@ package com.picpaysimplificado.picpaysimplificado.services;
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.picpaysimplificado.dtos.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,15 @@ public class NotificationService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${app.notificationApi}")
+    private String notificationApiUrl;
+
     public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
-//        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+        ResponseEntity<String> notificationResponse =
+                restTemplate.postForEntity(this.notificationApiUrl, notificationRequest, String.class);
 //
 //        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)){
 //            System.out.println("Error sending the notification.");
